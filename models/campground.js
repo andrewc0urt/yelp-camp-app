b67984cc61bb92b84471e5e9dc04b5f141f91919
andrewc0urt <andrew.court.dev@gmail.com>
2024-06-05 12:59:43 -0400

    Implemented Middleware to Automatically Delete Associated Reviews When a Campground Is Deleted
    Utilized Review.deleteMany to remove all reviews from the Review collection where the _id field matches the IDs in the campground's reviews array.
    
    Ensured data consistency by automatically cleaning up related reviews upon campground deletion.

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require('./review')
 
 const CampgroundSchema = new Schema({
   title: String,
@@ -14,6 +15,18 @@ const CampgroundSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+// Middleware to delete all associated reviews when a campground is deleted
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    // Remove all reviews from the Review collection where the _id field is in the doc reviews array
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
