6a11b38b5f8bdfe321e3df335d89a4c0dcd0960a
andrewc0urt <andrew.court.dev@gmail.com>
2024-07-11 10:32:41 -0400

    Refactored Routes Using router.route for Cleaner Code
    - Refactored route handling in routes/campgrounds.js and routes/users.js using router.route for improved code organization and reduced redundancy.
    
    - Replaced individual route declarations with chained methods of router.route() to match routes based on HTTP verbs (GET, POST, PUT, DELETE).
    
    - Enhanced readability and maintainability by centralizing route logic for each endpoint under a single router.route("/:id") declaration.
    
    - Applied the refactoring to both campground and user routes, ensuring consistency and clarity in route definitions across the application.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -9,19 +9,30 @@ const { validateCampground, isLoggedIn, isAuthor } = require("../middleware");
 // import the route logic (functions) from controllers
 const campgroundController = require("../controllers/campgrounds");
 
-router.get("/", catchAsync(campgroundController.index));
+// use router.route to avoid duplicate route naming; handles http verbs based on the single route it should it
+// below, the .get route and .post route have the route campgrounds/, but different HTTP verbs
+router.route("/").get(catchAsync(campgroundController.index)).post(isLoggedIn, validateCampground, catchAsync(campgroundController.createNewCampground));
+
+// router.get("/", catchAsync(campgroundController.index));
 
 router.get("/new", isLoggedIn, campgroundController.getNewCampgroundForm);
 
-router.post("/", isLoggedIn, validateCampground, catchAsync(campgroundController.createNewCampground));
+// router.post("/", isLoggedIn, validateCampground, catchAsync(campgroundController.createNewCampground));
+
+// use router.route to refactor the :id routes that are the same, but have different http verbs
+router
+  .route("/:id")
+  .get(catchAsync(campgroundController.showCampground))
+  .delete(isLoggedIn, isAuthor, catchAsync(campgroundController.deleteCampground))
+  .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgroundController.updateCampground));
 
 router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgroundController.getEditCampgroundForm));
 
-router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(campgroundController.updateCampground));
+// router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(campgroundController.updateCampground));
 
-router.delete("/:id", isLoggedIn, isAuthor, catchAsync(campgroundController.deleteCampground));
+// router.delete("/:id", isLoggedIn, isAuthor, catchAsync(campgroundController.deleteCampground));
 
-router.get("/:id", catchAsync(campgroundController.showCampground));
+// router.get("/:id", catchAsync(campgroundController.showCampground));
 
 // export the router to be used in router.js
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,15 +7,24 @@ const { storeReturnTo } = require("../middleware");
 
 const userController = require("../controllers/user");
 
-router.get("/register", userController.getRegistrationForm);
+// use router.route to refactor the /register routes with different http verbs
+router.route("/register").get(userController.getRegistrationForm).post(catchAsync(userController.registerUser));
 
-router.post("/register", catchAsync(userController.registerUser));
+// router.get("/register", userController.getRegistrationForm);
 
-router.get("/login", userController.getLoginForm);
+// router.post("/register", catchAsync(userController.registerUser));
+
+// user route.route to refactor the /log routes with different http verbs
+router
+  .route("/login")
+  .get(userController.getLoginForm)
+  .post(storeReturnTo, passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), userController.confirmLogin);
+
+// router.get("/login", userController.getLoginForm);
 
 // use the storeReturnTo middleware to save the returnTo value from session to res.locals
 // passport.authenticate logs the user in and clears req.session
-router.post("/login", storeReturnTo, passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), userController.confirmLogin);
+// router.post("/login", storeReturnTo, passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), userController.confirmLogin);
 
 router.get("/logout", userController.logoutUser);
 
