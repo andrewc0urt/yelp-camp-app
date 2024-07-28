4e5124850cf8cd5b806ab41efab769c65483d826
andrewc0urt <andrew.court.dev@gmail.com>
2024-07-28 17:27:10 -0400

    Changed Cluster Size and Color
    - Updated cluster size and color based on the number of campgrounds in each cluster.
    
    - Changed map style from dark-v11 to light-v11.

--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -4,7 +4,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map",
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-  style: "mapbox://styles/mapbox/dark-v11",
+  style: "mapbox://styles/mapbox/light-v11",
   center: [-103.5917, 40.6699],
   zoom: 3,
 });
@@ -31,11 +31,11 @@ map.on("load", () => {
     paint: {
       // Use step expressions (https://docs.mapbox.com/style-spec/reference/expressions/#step)
       // with three steps to implement three types of circles:
-      //   * Blue, 20px circles when point count is less than 100
-      //   * Yellow, 30px circles when point count is between 100 and 750
-      //   * Pink, 40px circles when point count is greater than or equal to 750
-      "circle-color": ["step", ["get", "point_count"], "#51bbd6", 100, "#f1f075", 750, "#f28cb1"],
-      "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
+      //   * Blue, 20px circles when point count is less than 10
+      //   * Yellow, 30px circles when point count is between 10 and 30
+      //   * Pink, 40px circles when point count is greater than or equal to 30
+      "circle-color": ["step", ["get", "point_count"], "#51bbd6", 10, "#f1f075", 30, "#f28cb1"],
+      "circle-radius": ["step", ["get", "point_count"], 15, 10, 20, 30, 25], // [step, size, step, size, size for everything greater than 30]
     },
   });
 
