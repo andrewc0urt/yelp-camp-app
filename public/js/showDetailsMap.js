973c6074eb87b382afc2a8fa9dad554ca42ee433
andrewc0urt <andrew.court.dev@gmail.com>
2024-07-31 18:41:42 -0400

    Refactored Mapbox Map Styling and Container IDs
    - Removed inline styling for the Mapbox cluster map and individual campground maps.
    
    - Created an app.css file for centralized map styling.
    
    - Updated map container IDs in the HTML.
    
    - Adjusted the container field in Mapbox initialization to match the new IDs for both maps.

--- public/js/showDetailsMap.js
+++ public/js/showDetailsMap.js
@@ -1,7 +1,7 @@
 // JavaScript for Using the Map via Mapbox
 mapboxgl.accessToken = mapToken; // maptoken comes from the first loaded script in showDetails.ejs
 const map = new mapboxgl.Map({
-  container: "map", // container ID
+  container: "showDetails-map", // container ID
   style: "mapbox://styles/mapbox/streets-v12", // style URL
   center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 12, // starting zoom
